940055bd85faab836895cfcb9c0594bfdfdb8f63
IbrahemFB <ibrahem800@gmail.com>
2022-05-19 12:36:18 +0200

    changed db port

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -10,7 +10,7 @@ export const config = {
   username: `${process.env.POSTGRES_USER}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port: Number(process.env.PORT),
+  port: Number(process.env.DB_PORT),
   db_port: Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
@@ -22,17 +22,3 @@ export const config = {
     secret: process.env.JWT_SECRET,
   },
 };
-
-// POSTGRES_HOST=localhost
-// POSTGRES_PORT=5432
-// DB_PORT=5432
-// POSTGRES_DB=udagram
-// POSTGRES_USER=postgres
-// POSTGRES_PASSWORD=postgres
-// AWS_REGION=XXXXXXXX
-// AWS_PROFILE=XXXXXXXX
-// AWS_BUCKET=XXXXXXXX
-// AWS_ACCESS_KEY_ID=XXXXXXXX
-// AWS_SECRET_ACCESS_KEY=XXXXXXXX
-// AWS_S3_ENDPOINT=XXXXXXXX
-// URL=localhost:8080
