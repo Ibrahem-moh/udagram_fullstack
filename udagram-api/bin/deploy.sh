5c54c65fed212bfd4c667b092ceff092a17fdf90
IbrahemFB <ibrahem800@gmail.com>
2022-05-19 23:04:40 +0200

    remove cd  deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,5 @@
 
-cd ..
-eb init --platform node.js-16 --region us-east-1 udaapp 
+ eb init --platform node.js-16 --region us-east-1 udaapp 
 eb use udaapp-env 
 eb deploy udaapp-env  
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_DEFAULT_REGION AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY JWT_SECRET=$JWT_SECRET POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD DB_PORT=$DB_PORT POSTGRES_USERNAME=$POSTGRES_USERNAME --profile default",
